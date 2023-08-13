067de4b38b3692faf39a0a68ea9391a65a4ec42b
seyifash <fasugbeoluwaseyi@gmail.com>
2023-08-13 15:21:09 -0700

    console test cases

--- tests/test_console.py
+++ tests/test_console.py
@@ -21,64 +21,52 @@ class TestHBNBCommand(unittest.TestCase):
         """test cases for prompt"""
         self.assertEqual('(hbnb) ', self.console.prompt)
 
-    # def test_help(self):
-    #     """test cases for help command"""
-    #     quit_help = "Quit command to exit the program"
-    #     with patch('sys.stdout', new=StringIO()) as f:
-    #         self.console.onecmd("help")
-    #         self.assertEqual(f.getvalue().strip(), quit_help)
-
     def test_create(self):
         """test cases for create command"""
         with patch('sys.stdout', new=StringIO()) as f:
-            """# Test case where class name is missing"""
+            """Test case where class name is missing"""
             self.console.onecmd("create")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** class name missing **")
 
-            """# Test case where class name doesn't exist"""
+            """Test case where class name doesn't exist"""
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("create MyModel")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** class doesn't exist **")
 
-            """# Test case where class name exists (BaseModel)"""
+            """Test case where class name exists (BaseModel)"""
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("create BaseModel")
             cmd_output = f.getvalue().strip()
             self.assertEqual(str, type(cmd_output))
 
-            """# Check if ID is printed"""
+            """ Check if ID is printed"""
             instance_id = cmd_output.split()[-1]
             self.assertIn(instance_id, cmd_output)
 
     def test_show(self):
         """test cases for show command"""
         with patch('sys.stdout', new=StringIO()) as f:
-            # Test case where class name is missing
             self.console.onecmd("show")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** class name missing **")
 
-            # Test case where class name doesn't exist
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("show MyModel")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** class doesn't exist **")
 
-            # Test case where instance id is missing
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("show BaseModel")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** instance id missing **")
 
-            # Test case where instance doesn't exist
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("show BaseModel 121212")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** no instance found **")
 
-            # Test case where class name exists and instance id exists
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("create BaseModel")
             cmd_output = f.getvalue().strip()
@@ -92,7 +80,7 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertIn(instance_id, show_output)
 
     def test_destroy(self):
-        """# Test destroy with valid instance"""
+        """Test destroy with valid instance"""
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("create BaseModel")
             cmd_output = f.getvalue().strip()
@@ -104,25 +92,21 @@ class TestHBNBCommand(unittest.TestCase):
 
             self.assertEqual(destroy_output, "")
 
-        # Test destroy with missing class name
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("destroy")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** class name missing **")
 
-        # Test destroy with non-existent class name
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("destroy SomeModel 1234-1234-1234")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** class doesn't exist **")
 
-        # Test destroy with missing instance ID
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("destroy BaseModel")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** instance id missing **")
 
-        # Test destroy with non-existent instance
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("destroy BaseModel 121212")
             error_output = f.getvalue().strip()
@@ -134,18 +118,15 @@ class TestHBNBCommand(unittest.TestCase):
         self.console.onecmd("create User")
         self.console.onecmd("create BaseModel")
 
-        # Retrieve all instances for a valid class name
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("all BaseModel")
             all_output = f.getvalue().strip()
 
-            # Check if the output matches the expected reult
             self.assertTrue("[BaseModel]" in all_output)
             self.assertTrue("'created_at':" in all_output)
             self.assertTrue("'id':" in all_output)
             self.assertTrue("'updated_at':" in all_output)
 
-        # Test all with non-existent class name
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("all SomeModel")
             error_output = f.getvalue().strip()
@@ -217,6 +198,59 @@ class TestHBNBCommand(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd('BaseModel.destroy("135)')
             self.assertEqual(f.getvalue().strip(), "** no instance found **")
+
+
+class TestHBNBCommand_help(unittest.TestCase):
+    """unittest for testing the help me for each of the command"""
+
+    def test_help_quit(self):
+        """Test cases for help quit command"""
+        quit_help = "Quit command to exit the program"
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help quit"))
+            self.assertEqual(quit_help, f.getvalue().strip())
+
+    def test_help_eof(self):
+        """test cases for help eof command"""
+        eof_help = "ctrl+D to exit the program"
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help EOF"))
+            self.assertEqual(eof_help, f.getvalue().strip())
+
+    def test_help_create(self):
+        """test cases for help eof command"""
+        create_help = "creates the instance of the variable"
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help create"))
+            self.assertEqual(create_help, f.getvalue().strip())
+
+    def test_help_update(self):
+        """test case for the helf update command"""
+        update_help = "updates an attribute in an object"
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help update"))
+            self.assertEqual(update_help, f.getvalue().strip())
+
+    def test_help_destroy(self):
+        """test case for the helf destroy command"""
+        destroy_help = "destroys an object from the json database"
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help destroy"))
+            self.assertEqual(destroy_help, f.getvalue().strip())
+
+    def test_help_count(self):
+        """test case for the helf count command"""
+        count_help = "counts the number of intsnaces of an object"
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help count"))
+            self.assertEqual(count_help, f.getvalue().strip())
+
+    def test_help_show(self):
+        """test case for the helf destroy command"""
+        show_help = "shows the object based on id"
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("help show"))
+            self.assertEqual(show_help, f.getvalue().strip())
 
 
 if __name__ == "__main__":
