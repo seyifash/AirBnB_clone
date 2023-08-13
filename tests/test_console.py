c5ac61d686df66d143963f25452dd6d0cc1f90ef
Osei <williamsabodunrin@gmail.com>
2023-08-13 16:01:45 +0000

    test cases

--- tests/test_console.py
+++ tests/test_console.py
@@ -6,13 +6,25 @@ import unittest
 from unittest.mock import patch
 from io import StringIO
 from console import HBNBCommand
+from models.engine.file_storage import FileStorage
 
 
 class Test_HBNBCommandConsole(unittest.TestCase):
-    
+
     def setUp(self):
         self.console = HBNBCommand()
+        self.storage_cpy = FileStorage()
 
+    def test_prompt(self):
+        """test cases for prompt"""
+        self.assertEqual('(hbnb) ', self.console.prompt)
+
+    # def test_help(self):
+    #     """test cases for help command"""
+    #     quit_help = "Quit command to exit the program"
+    #     with patch('sys.stdout', new=StringIO()) as f:
+    #         self.console.onecmd("help")
+    #         self.assertEqual(f.getvalue().strip(), quit_help)
 
     def test_create(self):
         with patch('sys.stdout', new=StringIO()) as f:
@@ -22,14 +34,16 @@ class Test_HBNBCommandConsole(unittest.TestCase):
             self.assertEqual(error_output, "** class name missing **")
 
             # Test case where class name doesn't exist
+        with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("create MyModel")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** class doesn't exist **")
 
             # Test case where class name exists (BaseModel)
+        with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("create BaseModel")
             cmd_output = f.getvalue().strip()
-            self.assertIn("BaseModel", cmd_output)
+            self.assertEqual(str, type(cmd_output))
 
             # Check if ID is printed
             instance_id = cmd_output.split()[-1]
@@ -43,25 +57,30 @@ class Test_HBNBCommandConsole(unittest.TestCase):
             self.assertEqual(error_output, "** class name missing **")
 
             # Test case where class name doesn't exist
+        with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("show MyModel")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** class doesn't exist **")
 
             # Test case where instance id is missing
+        with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("show BaseModel")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** instance id missing **")
 
             # Test case where instance doesn't exist
+        with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("show BaseModel 121212")
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** no instance found **")
 
             # Test case where class name exists and instance id exists
+        with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("create BaseModel")
             cmd_output = f.getvalue().strip()
             instance_id = cmd_output.split()[-1]
 
+        with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd(f"show BaseModel {instance_id}")
             show_output = f.getvalue().strip()
 
@@ -75,6 +94,7 @@ class Test_HBNBCommandConsole(unittest.TestCase):
             cmd_output = f.getvalue().strip()
             instance_id = cmd_output.split()[-1]
 
+        with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd(f"destroy BaseModel {instance_id}")
             destroy_output = f.getvalue().strip()
 
@@ -104,12 +124,11 @@ class Test_HBNBCommandConsole(unittest.TestCase):
             error_output = f.getvalue().strip()
             self.assertEqual(error_output, "** no instance found **")
 
-
-
     def test_all(self):
         # Create instances
-        self.console.onecmd("create BaseModel")
+
         self.console.onecmd("create User")
+        self.console.onecmd("create BaseModel")
 
         # Retrieve all instances for a valid class name
         with patch('sys.stdout', new=StringIO()) as f:
@@ -117,8 +136,8 @@ class Test_HBNBCommandConsole(unittest.TestCase):
             all_output = f.getvalue().strip()
 
             # Check if the output matches the expected format
-            self.assertTrue(all_output.startswith("[BaseModel]"))
-            self.assertTrue("{'created_at':" in all_output)
+            self.assertTrue("[BaseModel]" in all_output)
+            self.assertTrue("'created_at':" in all_output)
             self.assertTrue("'id':" in all_output)
             self.assertTrue("'updated_at':" in all_output)
 
@@ -142,6 +161,55 @@ class Test_HBNBCommandConsole(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             self.console.onecmd("")
             self.assertEqual(f.getvalue().strip(), "")
+
+    def test_default(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            uk = "jkj"
+            self.console.onecmd(uk)
+            output = f.getvalue().strip()
+            self.assertEqual(output, "*** Unknown syntax: {}".format(uk))
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("create BaseModel")
+            id_var = f.getvalue().strip()
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd("BaseModel.all()")
+            output = f.getvalue().strip()
+            self.assertEqual(output[0], "[")
+            self.assertEqual(output[-1], "]")
+            self.assertTrue(id_var in output)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd('BaseModel.show("{}")'.format(id_var))
+            output = f.getvalue().strip()
+            self.assertTrue(id_var in output)
+            self.assertTrue(output.startswith("[BaseModel]"))
+            self.assertIn("datetime", output)
+            self.assertIn("updated_at", output)
+            self.assertIn("created_at", output)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd('BaseModel.show()')
+            self.assertEqual(f.getvalue().strip(), "** instance id missing **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd('BaseModel.show("135)')
+            self.assertEqual(f.getvalue().strip(), "** no instance found **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            all_objs = len(self.storage_cpy.all())
+            self.console.onecmd('BaseModel.destroy("{}")'.format(id_var))
+            self.assertEqual(len(self.storage_cpy.all()), all_objs - 1)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd('BaseModel.destroy()')
+            self.assertEqual(f.getvalue().strip(), "** instance id missing **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd('BaseModel.destroy("135)')
+            self.assertEqual(f.getvalue().strip(), "** no instance found **")
+
 
 if __name__ == "__main__":
     unittest.main()
